a383b4e7f8b36707dff16259bce9752ebd726c52
NimishJain <110347787+nimishjain394@users.noreply.github.com>
2022-07-31 22:33:17 +0530

    Update streamlit-app.py
    Updated the menu in the code.

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('This is my first streamlit code')
+
+streamlit.header('Todays Menu')
+streamlit.text('Tarri Poha')
+streamlit.text('Chola Samosa')
+streamlit.text('Idli Sambar')
